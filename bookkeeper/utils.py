f13687a1042c55caf501f84c189b8107aff83f92
tlebchan <gleb.khegai@bostongene.com>
2023-04-06 18:28:51 +0400

    utils corrected

--- bookkeeper/utils.py
+++ bookkeeper/utils.py
@@ -2,7 +2,10 @@
 Вспомогательные функции
 """
 
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, Any
+from bookkeeper.models.expense import Expense
+from bookkeeper.models.category import Category
+from bookkeeper.models.budget import Budget
 
 
 def _get_indent(line: str) -> int:
@@ -21,23 +24,18 @@ def read_tree(lines: Iterable[str]) -> list[tuple[str, str | None]]:
     Прочитать структуру дерева из текста на основе отступов. Вернуть список
     пар "потомок-родитель" в порядке топологической сортировки. Родитель
     элемента верхнего уровня - None.
-
     Пример. Следующий текст:
     parent
         child1
             child2
         child3
-
     даст такое дерево:
     [('parent', None), ('child1', 'parent'),
      ('child2', 'child1'), ('child3', 'parent')]
-
     Пустые строки игнорируются.
-
     Parameters
     ----------
     lines - Итерируемый объект, содержащий строки текста (файл или список строк)
-
     Returns
     -------
     Список пар "потомок-родитель"
@@ -61,3 +59,56 @@ def read_tree(lines: Iterable[str]) -> list[tuple[str, str | None]]:
         last_name = name
         last_indent = indent
     return result
+
+
+def expense_adapter(exp_row: dict[str, Any]) -> Expense:
+    """
+    Адаптер для базы с тратами
+    Parameters
+    ----------
+    exp_row - строка в базе с тратами в виде словаря
+    Returns
+    -------
+    Expense объект с данными из базы
+    """
+    return Expense(pk=int(exp_row['pk']), amount=int(exp_row['amount']),
+                   expense_date=exp_row['expense_date'],
+                   added_date=exp_row['added_date'],
+                   comment=exp_row['comment'], category=int(exp_row['category']))
+
+
+def category_adapter(cat_row: dict[str, Any]) -> Category:
+    """
+    Адаптер для базы с категориями
+    Parameters
+    ----------
+    cat_row - строка в базе с категориями в виде словаря
+    Returns
+    -------
+    Category объект с данными из базы
+    """
+    return Category(pk=cat_row['pk'], name=cat_row['name'],
+                    parent=cat_row['parent'])
+
+
+def budget_adapter(budget_row: dict[str, Any]) -> Budget:
+    """
+    Адаптер для базы с бюджетом
+    Parameters
+    ----------
+    budget_row - строка в базе с бюджетом в виде словаря
+    Returns
+    -------
+    Budget объект с данными из базы
+    """
+    return Budget(pk=int(budget_row['pk']),
+                  budget=int(budget_row['budget']),
+                  cur_sum=int(budget_row['cur_sum'])
+                  )
+
+
+adapters = {
+    'budget': budget_adapter,
+    'expense': expense_adapter,
+    'category': category_adapter
+}
